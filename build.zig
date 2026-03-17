463232f002f58a7a66e0913636983e0d1a291d37
Zeno343 <zacheryklinglesmith@gmail.com>
2026-03-16 23:44:31 -0400

    build 2_triangles

--- build.zig
+++ build.zig
@@ -25,6 +25,19 @@ pub fn build(b: *std.Build) void {
     lib.linkSystemLibrary("gl");
     lib.linkLibC();
 
+    const tris = b.addExecutable(.{
+        .name = "2_triangles",
+        .root_module = b.addModule("2_triangles", .{
+            .root_source_file = b.path("examples/2_triangles.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    tris.linkSystemLibrary("sdl3");
+    tris.linkSystemLibrary("gl");
+    tris.root_module.addImport("catchfire", lib.root_module);
+    tris.linkLibC();
+
     bin.linkSystemLibrary("sdl3");
     bin.linkSystemLibrary("gl");
     bin.root_module.addImport("catchfire", lib.root_module);
@@ -32,11 +45,17 @@ pub fn build(b: *std.Build) void {
 
     const install = b.addInstallArtifact(bin, .{});
     const install_lib = b.addInstallArtifact(lib, .{});
+    const install_2_tris = b.addInstallArtifact(tris, .{});
 
     b.getInstallStep().dependOn(&install_lib.step);
+    b.getInstallStep().dependOn(&install_2_tris.step);
     b.getInstallStep().dependOn(&install.step);
 
     const run = b.addRunArtifact(bin);
+    const run_2_tris = b.addRunArtifact(tris);
     run.step.dependOn(&install.step);
+    run_2_tris.step.dependOn(&install.step);
+
+    b.step("2_tris", "2 triangles").dependOn(&run_2_tris.step);
     b.step("run", "run the editor").dependOn(&run.step);
 }
---
+++ examples/2_triangles.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+
+const ctcf = @import("catchfire");
+
+pub fn main() !void {
+    std.debug.print("hello world\n", .{});
+}
