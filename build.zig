be7a385ed7642ad58b157f960e9a905eba93c67f
Zeno343 <zacheryklinglesmith@gmail.com>
2026-03-16 22:04:11 -0400

    split library

--- build.zig
+++ build.zig
@@ -4,20 +4,22 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const main = b.addModule("engine", .{
-        .root_source_file = b.path("src/main.zig"),
+    const bin = b.addExecutable(.{ 
+        .name = "catchfire", 
+        .root_module = b.addModule("catchfire", .{ 
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    const lib = b.addLibrary(.{ 
+        .name = "catchfire", 
+        .root_module = b.addModule("catchfire", .{
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
-    });
-
-    const bin = b.addExecutable(.{
-        .name = "catchfire",
-        .root_module = main,
-    });
-
-    const lib = b.addLibrary(.{
-        .name = "libCatchfire",
-        .root_module = main,
+      }), 
     });
     lib.linkSystemLibrary("sdl3");
     lib.linkSystemLibrary("gl");
@@ -25,9 +27,13 @@ pub fn build(b: *std.Build) void {
 
     bin.linkSystemLibrary("sdl3");
     bin.linkSystemLibrary("gl");
+    bin.root_module.addImport("catchfire", lib.root_module);
     bin.linkLibC();
 
     const install = b.addInstallArtifact(bin, .{});
+    const install_lib = b.addInstallArtifact(lib, .{});
+
+    b.getInstallStep().dependOn(&install_lib.step);
     b.getInstallStep().dependOn(&install.step);
 
     const run = b.addRunArtifact(bin);
